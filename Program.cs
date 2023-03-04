7f788f6db8797beb4f7b183c55e701bf113d8a9b
XxSweet <xxsweet07@gmail.com>
2023-03-03 21:53:07 -0300

    alteraçao no nome

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("---Minhas frutas preferidas---");
+﻿Console.WriteLine("---Minhas frutas preferidas---");
 Console.WriteLine();
 Console.WriteLine("1. Abacate");
 Console.WriteLine("2. Goiaba");
